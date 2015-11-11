cbe713343ca8b88ecd7244bba572c009e8278448
nmlbrar <anmolbrar288@gmail.com>
2015-11-11 12:56:15 -0800

    maps
    I have added the maps :+1:

---
+++ js/js.js
@@ -0,0 +1,41 @@
+//map 1 for stylist 
+$(document).ready(function(){
+        window.onclick = function(event){
+            console.log(event);
+        }
+   });
+        console.log(event);
+                function loadMyMap(){
+                    console.log(google);
+                    var map = new google.maps.Map(
+                        document.getElementById("gogmap"),
+                        {
+                            center:{
+                                lat:49,
+                                lng:-123
+                            },
+                            zoom:10
+                        }
+
+                    )};
+
+// map2 for customers 
+$(document).ready(function(){
+        window.onclick = function(event){
+            console.log(event);
+        }
+   });
+        console.log(event);
+                function loadMyMap(){
+                    console.log(google);
+                    var map = new google.maps.Map(
+                        document.getElementById("gogmap2"),
+                        {
+                            center:{
+                                lat:49,
+                                lng:-123
+                            },
+                            zoom:10
+                        }
+
+                    )};
