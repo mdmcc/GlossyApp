2c8662095956d1f11c37831b7a113fc120af9200
nmlbrar <anmolbrar288@gmail.com>
2015-11-17 03:17:58 -0800

    improvements in api's
    changed the facebook login button.. Last one wasn't good with using
    FB.login function..

--- js/customerFB_api.js
+++ js/customerFB_api.js
@@ -1,4 +1,4 @@
-
+ var info = [];
   window.fbAsyncInit = function() {
     FB.init({
       appId      : '525265574303594',
@@ -6,15 +6,42 @@
       version    : 'v2.5'
     });
    
-      var loginBut = document.getElementById("fb-login-button"); 
+    var fbButLogin = document.getElementById("fbLoginBut");
       FB.getLoginStatus(function(response1){
           console.log(response1);
           if(response1.status == "connected"){
-              FB.api("/me/", function(response2){
+              FB.api('/me?fields=first_name,last_name,gender,about,age_range', function(response2){
+        
                   console.log(response2);
+                    info.push(response2);
+                    console.log(info);
+                  
+                var inFname = document.getElementById("firstname");
+                    //console.log(info[0].name);
+                    inFname.value = info[0].first_name;
+                  
+                var inLname = document.getElementById("lastname");
+                    inLname.value = info[0].last_name;
               })
           }
       })
+
+     fbButLogin.onclick = function(evt){
+                      FB.login(function(resp){
+                        console.log(resp);
+                        if(resp.status == "unknown"){
+                            //alert("Go away");
+                        }
+                        if(resp.status == "connected"){
+                           // alert("yayyy");
+                            location.reload();
+                            
+                        }
+                    },{
+                          scope:"user_about_me,email"
+                      });
+                  }
+                  //console.log(FB); 
   };
 
   (function(d, s, id){
@@ -24,3 +51,5 @@
      js.src = "//connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
+
+    
