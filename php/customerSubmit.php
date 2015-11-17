58d33f904989891cd57844d027f750c4b2c2cbd8
JustineH <psmyrniotis@justines-air.wifi.bcit.ca>
2015-11-17 10:11:39 -0800

    commit me

--- php/customerSubmit.php
+++ php/customerSubmit.php
@@ -1,13 +1,14 @@
 <?php
 
     //var_dump($_POST);
-    $con = mysqli_connect("localhost”, “kmcin”, “GlossyApp2”);
-
+    $connect = mysql_connect("localhost", "root", "root", "glossy");
+    if (!connect) { die('Connection Failed: ' . mysql_error()); { mysql_select_db(“glossy”, $connect);
+    include("sessions.php");
     //var_dump($_POST["customer_dateTime"]);
     //$_POST["customer_dateTime"] = date($_POST["customer_dateTime"]);
 
     $sql = "insert into appointment (service_id, customer_id, customer_price, customer_dateTime, customer_lng, customer_lat, appointment_status) values ('".$_POST["service_id"]."', '".$_POST["customer_id"]."', '".$_POST["customer_price"]."', '".$_POST["customer_dateTime"]."', '".$_POST["customer_lng"]."', '".$_POST["customer_lat"]."', '0')";
-    $results = mysqli_query ($con, $sql);
+    $results = mysqli_query ($connect, $sql);
     if($results){
 
         echo json_encode("success");
