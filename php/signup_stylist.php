92588351122c25e4923e208469298124c4e5aeac
nmlbrar <anmolbrar288@gmail.com>
2015-11-18 15:42:51 -0800

    Please Double check I might be wrong
    the stylist form was pushing info into Customer table I guess

--- php/signup_stylist.php
+++ php/signup_stylist.php
@@ -3,7 +3,7 @@ include 'connection.php';
 
 mysql_select_db("glossy", $connect);
 
- $user_info = "INSERT INTO customer (hairStylist_firstName, hairStylist_lastName, hairStylist_email, hairStylist_password, hairStylist_confirmpass) VALUES ('".$_POST['firstname']."', '".$_POST['lastname']."', '".$_POST['email']."', '".$_POST['password']."', '".$_POST['confirmPassword']."')";  
+ $user_info = "INSERT INTO hairStylist (hairStylist_firstName, hairStylist_lastName, hairStylist_email, hairStylist_password, hairStylist_confirmpass) VALUES ('".$_POST['firstname']."', '".$_POST['lastname']."', '".$_POST['email']."', '".$_POST['password']."', '".$_POST['confirmPassword']."')";  
 echo "query made";
 $result = mysql_query($user_info);
 if($result)
