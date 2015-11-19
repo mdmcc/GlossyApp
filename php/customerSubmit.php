57118bafca4e83831cf37e3b5bddb76e9f7212b2
JustineH <psmyrniotis@Justines-MacBook-Air.local>
2015-11-18 23:17:09 -0800

    Trying to connect php to database

--- php/customerSubmit.php
+++ php/customerSubmit.php
@@ -2,17 +2,17 @@
 
     //var_dump($_POST);
     
-    $connect = mysqli_connect("localhost", "glossyadmin1", "glossy"); 
+    $connect = mysql_connect("localhost", "glossyadmin1", "glossy"); 
     
-    if (!$connect) { die('Connection Failed: ' . mysqli_error()); }
-    mysqli_select_db(“glossy”, $connect);
+    if (!$connect) { die('Connection Failed: ' . mysql_error()); }
+    mysql_select_db(“glossy”, $connect);
 
     // include("sessions.php");
     //var_dump($_POST["customer_dateTime"]);
     //$_POST["customer_dateTime"] = date($_POST["customer_dateTime"]);
 
     $sql = "insert into appointment (service_id, customer_id, customer_price, customer_dateTime, customer_lng, customer_lat, appointment_status) values ('".$_POST["service_id"]."', '".$_POST["customer_id"]."', '".$_POST["customer_price"]."', '".$_POST["customer_dateTime"]."', '".$_POST["customer_lng"]."', '".$_POST["customer_lat"]."', '0')";
-    $results = mysqli_query ($connect, $sql);
+    $results = mysql_query ($connect, $sql);
     
     if($results){
         echo json_encode("success");
