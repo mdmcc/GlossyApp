81c39a04cab8d5b123f5d64ad4f3b271ee5f20e7
JustineH <psmyrniotis@Justines-MacBook-Air.local>
2015-11-18 23:20:03 -0800

    $ updated

--- php/connection.php
+++ php/connection.php
@@ -1,4 +1,4 @@
 <?php
 $connect = mysql_connect("localhost", "glossyadmin1", "glossy"); 
-if (!connect) { die('Connection Failed: ' . mysql_error());}
+if (!$connect) { die('Connection Failed: ' . mysql_error());}
 ?>
