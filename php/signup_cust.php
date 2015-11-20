52367a216460b085b605f9d542832cdd6066cf02
JustineH <psmyrniotis@Justines-MacBook-Air.local>
2015-11-20 03:58:34 -0800

    Added $connect to connect to host

--- php/signup_cust.php
+++ php/signup_cust.php
@@ -2,6 +2,7 @@
 include("connection.php");
 
 // mysql_select_db("glossy", $connect);
+$connect = mysqli_connect("localhost", "glossyadmin1", "glossy"); 
 
  $user_info = "INSERT INTO customer (customer_firstName, customer_lastName, customer_email, customer_password, customer_confirmpass) VALUES ('".$_POST['firstname']."', '".$_POST['lastname']."', '".$_POST['email']."', '".$_POST['password']."', '".$_POST['confirmPassword']."')";  
 echo "query made";
