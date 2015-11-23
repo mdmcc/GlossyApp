9b6249a816a9f615919a68c00258e80091b61505
JustineH <psmyrniotis@justines-air.wifi.bcit.ca>
2015-11-23 11:33:49 -0800

    Removed customer_services.html, updated database.txt, etc.

--- php/customerSubmit.php
+++ php/customerSubmit.php
@@ -1,27 +1,90 @@
 <?php
     include("connection.php");
-
-    //var_dump($_POST);
     
-    $connect = mysqli_connect("localhost", "glossyadmin1", "glossy"); 
+    $connect = mysqli_connect("localhost", "root", "root", "glossy"); 
     
     if(mysqli_connect_errno()){
-    echo "Something went wrong connecting... " . mysqli_connect_error();
-    exit();
+        echo "Something went wrong connecting... " . mysqli_connect_error();
+    // exit();
     }
-    // mysql_select_db("glossy", $connect);
-    
-    // include("sessions.php");
-    //var_dump($_POST["customer_dateTime"]);
-    //$_POST["customer_dateTime"] = date($_POST["customer_dateTime"]);
 
-    $sql = "INSERT INTO appointment (service_id, customer_id, customer_price, customer_dateTime, customer_lng, customer_lat, appointment_status) values ('".$_POST["service_id"]."', '".$_POST["customer_id"]."', '".$_POST["customer_price"]."', '".$_POST["customer_dateTime"]."', '".$_POST["customer_lng"]."', '".$_POST["customer_lat"]."', '0')";
+    // First is to insert Appointment Service data from POST 
+    // To get an ID for our relation
+    $insertAppointmentServiceSql = "INSERT INTO appointment_service (cut_type, color_type, styling_type) VALUES (" . $_POST['cut_type'] . ", " . $_POST['color_type'] . ", " . $_POST['styling_type'] . ")";
+    $appointmentServiceResults = mysqli_query ($connect, $insertAppointmentServiceSql);
+    $insertAppointmentInsertId = mysqli_insert_id($connect);
+
+    // Second, get POST data to insert appointment data
+    $insertAppointmentSql = "INSERT INTO appointment (customer_id, appointment_service_id, customer_dateTime, customer_lng, customer_lat, hairStylist_id, appointment_status) VALUES (" . $_POST['customer_id'] . ", " . $insertAppointmentInsertId  . ", '" . $_POST['customer_dateTime'] . "', " . $_POST['customer_lng'] . ", " . $_POST['customer_lat'] . ", " . $_POST['hairStylist_id'] . ", 0)";
+
+    $results = mysqli_query ($connect, $insertAppointmentSql);
+
+    // $cut = null;
+    // $color = null;
+    // $styling = null;
+
+
+    // if($_POST['cut']) {
+    //     $cut = $_POST['cut'];
+    // }
+    //  if($_POST['color']) {
+    //     $color = $_POST['color'];
+    // }
+    //  if($_POST['styling']) {
+    //     $styling = $_POST['styling'];
+    // }
+
+
+    // $cost = 0;
+
+    // if($cut) {
+    //     $cut_type = $cut['simpleCut'];
+
+    //     if($cut_type == "simpleCut") {
+    //         $cost += 50;
+
+    //     } else if($cut_type == "premiumCut") {
+    //         $cost += 90;
+    //     }
+    // }
+
+    // if($color) {
+    //     $color_type = $color['simpleColor'];
+
+    //     if($color_type == "simpleColor") {
+    //         $cost += 70;
+
+    //     } else if($cut_type == "premiumColor") {
+    //         $cost += 140;
+    //     }
+    // }
+
+    //  if($styling) {
+    //     $styling_type = $styling['simpleStyling'];
+
+    //     if($styling_type == "simpleStyling") {
+    //         $cost += 50;
+
+    //     } else if($styling_type == "premiumStyling") {
+    //         $cost += 70;
+    //     }
+    // }
+
+    //$appointment = getAppointmentStuff($_POST);
+
+    // $sql = "INSERT INTO appointment (customer_id, appointment_service_id, customer_dateTime, customer_lng, customer_lat, hairStylist_id, appointment_status) values ('".$_POST["customer_id"]."', '".$_POST["appointment_service_id"]."', '".$_POST["customer_dateTime"]."', '".$_POST["customer_lng"]."', '".$_POST["customer_lat"]."', '0', '0')";
+    // $sql = "INSERT INTO appointment (service_id, customer_id, customer_price, customer_dateTime, customer_lng, customer_lat, appointment_status) values ('".$_POST["service_id"]."', '".$_POST["customer_id"]."', '".$_POST["customer_price"]."', '".$_POST["customer_dateTime"]."', '".$_POST["customer_lng"]."', '".$_POST["customer_lat"]."', '0')";
+
     $results = mysqli_query ($connect, $sql);
-
-    // $results = mysqli_query ($connect, $sql);
     
     if($results){
         echo json_encode("success");
     };
+
+    // function getAppointmentStuff($input) {
+    //     // Get data from appointment_service table
+    //     // return it
+    //     $sql = "SELECT * FROM appointment_service WHERE cut_type ";
+    // }
 
   ?>  
--- php/fbsignup_cust.php
+++ php/fbsignup_cust.php
@@ -1,9 +1,11 @@
 <?php
-echo "hey";
-$connect = mysqli_connect("localhost", "glossyadmin1", "glossy");  
-// mysql_select_db("glossy_test", $connect);
 
- $user_info = "INSERT INTO customer (customer_firstName, customer_lastName, customer_email, customer_password, customer_confirmpass) VALUES ('".$_POST['firstname']."', '".$_POST['lastname']."', '".$_POST['email']."', '".$_POST['password']."', '".$_POST['confirmPassword']."')";  
+include("connection.php");
+
+echo "hey";
+$connect = mysqli_connect("localhost", "root", "root", "glossy");  
+
+$user_info = "INSERT INTO customer (customer_email, customer_password, customer_confirmpass, customer_firstName, customer_lastName) VALUES ('".$_POST['email']."', '".$_POST['password']."', '".$_POST['confirmPassword']."', '".$_POST['firstname']."', '".$_POST['lastname']."')";  
 echo "helloo";
 
 if (!mysqli_query($user_info)) { die('Error: ' . mysqli_error()); };
