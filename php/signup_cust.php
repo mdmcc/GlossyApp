33c863730c4b97c60148158a68e3368b9712f5e1
trinity546 <katymcintosh@katys-mbp.wifi.bcit.ca>
2015-11-25 11:30:31 -0800

    Merge remote-tracking branch 'origin/glossy2.0' into glossy2.0
    # Conflicts:
    #	customer_signup_login.html
    #	stylist_signup_login.html

--- php/signup_cust.php
+++ php/signup_cust.php
@@ -1,16 +1,15 @@
 <?php
 include("connection.php");
 
+<<<<<<< Updated upstream
 $user_info = "INSERT INTO customer (customer_email, customer_password, customer_confirmpass, customer_firstName, customer_lastName) VALUES ('".$_POST['email']."', '".$_POST['password']."', '".$_POST['confirmPassword']."', '".$_POST['firstname']."', '".$_POST['lastname']."')";  
 
+=======
+// mysqli_select_db("glossy", $connect);
+>>>>>>> Stashed changes
 
 $result = mysqli_query($connect, $user_info);
-if($result)
-{
-	//echo "Your information was added to the database.";
-}
-else {
-	echo mysqli_error();
-}
+
+
 header("Location:/customer_appointment.html");
-?>                                                       
+?>  
