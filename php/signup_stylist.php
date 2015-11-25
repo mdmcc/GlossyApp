8f73b7f74b4884d18403bf20953c39b4686e6153
nmlbrar <anmolbrar288@gmail.com>
2015-11-24 16:08:49 -0800

    back button for customer_signup_login is working

--- php/signup_stylist.php
+++ php/signup_stylist.php
@@ -2,11 +2,12 @@
 include("connection.php");
 
 $user_info = "INSERT INTO hairStylist (hairStylist_email, hairStylist_password, hairStylist_confirmpass, hairStylist_firstName, hairStylist_lastName) VALUES ('".$_POST['email']."', '".$_POST['password']."', '".$_POST['confirmPassword']."', '".$_POST['firstname']."', '".$_POST['lastname']."')";  
-echo "query made";
+//echo "query made";
 $result = mysqli_query($connect, $user_info);
 if($result)
 {echo "Your information was added to the database.";}
 else {
 	echo mysqli_error();
 }
+
 ?> 
