210b2c38947849e948b670503ea85503130d122d
trinity546 <katymcintosh@katys-mbp.wifi.bcit.ca>
2015-12-01 16:16:26 -0800

    fixing hopefully

--- php/login_cust.php
+++ php/login_cust.php
@@ -2,21 +2,31 @@
 include("connection.php");
 include("sessions.php");
 
-if(mysqli_connect_errno()){
-        echo "Something went wrong connecting... " . mysqli_connect_error();
-    // exit();
-}
 
-$userSQL = "SELECT * FROM customer WHERE customer_email = '".$_POST['email']."' AND customer_password = '". $_POST['password'] ."'";
+$username = $_POST['email'];
+$password = $_POST['password'];
 
-$results = mysqli_query($connect, $userSQL);
 
-if($results){
-	$row = mysqli_fetch_array($results);
+$user_info = 'SELECT customer_email, customer_password FROM customer 
+ WHERE customer_password = "'.$password.'" AND customer_email = "'.$username.'"';
+
+$result = mysqli_query($connect, $user_info);
+
+
+
+if($result){
+	if (mysqli_num_rows($result) > 0) {
 	$_SESSION['login'] = true;
 	$_SESSION['email'] = $_POST['email'];
-    $_SESSION['id'] = $row['ID'];
-    //echo json_encode("success");
-    header("Location:/GlossyApp/customer_appointment.html");	
+	echo json_encode("success");
+	} else {
+		echo json_encode("Your username or password is wrong");	
+	}
+	
+	
+} else {
+	$_SESSION['error'] = "Your username or password does not match";
+	echo json_encode("error");	
+	
+	
 }
-?>
--- php/login_stylist.php
+++ php/login_stylist.php
@@ -2,48 +2,29 @@
 include("connection.php");
 include("sessions.php");
 
+$username = $_POST['email'];
+$password = $_POST['password'];
 
-if(mysqli_connect_errno()){
-        echo "Something went wrong connecting... " . mysqli_connect_error();
-    // exit();
-}	
 
-$userSQL = "SELECT * FROM hairStylist WHERE hairStylist_email = '".$_POST['email']."' AND hairStylist_password = '". $_POST['password'] ."'";
+$user_info = 'SELECT hairStylist_email, hairStylist_password FROM hairStylist 
+ WHERE hairStylist_password = "'.$password.'" AND hairStylist_email = "'.$username.'"';
 
-$results = mysqli_query($connect, $userSQL);
+$result = mysqli_query($connect, $user_info);
 
-if($results){
-	$row = mysqli_fetch_array($results);
+
+if($result){
+	if (mysqli_num_rows($result) > 0) {
 	$_SESSION['login'] = true;
 	$_SESSION['email'] = $_POST['email'];
-    $_SESSION['id'] = $row['ID'];
-    //echo json_encode("success");
-    header("Location:/GlossyApp/stylist_calendar.html");	
+	echo json_encode("success");
+	} else {
+		echo json_encode("Your username or password is wrong");	
+	}
+
+} else {
+	$_SESSION['error'] = "Your username or password does not match";
+	echo json_encode("error");	
+	
 }
 
-// $username = "guest";
-// $password = "password";
-
-// if($username == $_POST['email'] && $password == $_POST['password']){
-// 	$_SESSION['login'] = true;
-// 	$_SESSION['email'] = $_POST['email'];
-
-
-// 	$userSQL = "SELECT * FROM hairStylist WHERE email = {$_SESSION['email']}";
-
-// 	$results = mysqli_query($connect, $userSQL);
-
-// 	if($result){
-// 		$row = mysqli_fetch_array($result);
-// 	    $_SESSION['id'] = $row['ID'];	
-// 	}	
-	
-// } else {
-// 	$_SESSION['login'] = false;
-// 	echo "Please first login.";	
-// }
-
-//     if($results){
-//         echo json_encode("success");
-//     };
 ?>
